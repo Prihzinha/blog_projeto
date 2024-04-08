dae6055adae6b35b092b348e405d8c75432bc3b0
Prihzinha <priscilaraguiar@gmail.com>
2024-04-08 11:21:10 -0300

    version 5

--- project/home/forms.py
+++ project/home/forms.py
@@ -4,7 +4,7 @@ from .models import Profile, BlogPost
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ('phone_no', 'bio', 'facebook', 'instagram', 'linkedin', 'image')
+        fields = ('bio', 'facebook', 'instagram', 'linkedin', 'image')
         
        
 class BlogPostForm(forms.ModelForm):
