aef8a63b7970300ecae0d97861ecb8023b908d5d
Josue23 <josuerodrigues@gmail.com>
2024-04-12 20:25:26 -0300

    melhorias na edição do post

--- home/forms.py
+++ home/forms.py
@@ -2,19 +2,30 @@ from django import forms
 from .models import Profile, BlogPost
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
+
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('bio', 'facebook', 'instagram', 'linkedin', 'image')
-        
-       
+
+
 class BlogPostForm(forms.ModelForm):
-    content = forms.CharField(widget=CKEditorUploadingWidget(attrs={'placeholder': 'Digite o conteúdo aqui...'}))
+    content = forms.CharField(
+        widget=CKEditorUploadingWidget(attrs={
+            'placeholder': 'Digite o conteúdo aqui...'}
+        )
+    )
 
     class Meta:
         model = BlogPost
         fields = ('title', 'slug', 'content', 'image')
         widgets = {
-            'title': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Título do Blog'}),
-            'slug': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Coloque a palavra chave do post sem espaço e separado com hífen'}),
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Título do Blog',
+                'autofocus': 'autofocus',
+            }),
+            'slug': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Coloque a palavra chave do post sem espaço e separado com hífen'}),
         }
--- home/models.py
+++ home/models.py
@@ -4,44 +4,47 @@ from django.urls import reverse
 from django.utils.timezone import now
 from ckeditor_uploader.fields import RichTextUploadingField
 
-    
+
 class Profile(models.Model):
     id = models.AutoField(primary_key=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="profile_pics", blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
     phone_no = models.IntegerField(blank=True, null=True)
     facebook = models.CharField(max_length=300, blank=True, null=True)
     instagram = models.CharField(max_length=300, blank=True, null=True)
     linkedin = models.CharField(max_length=300, blank=True, null=True)
-    
+
     def __str__(self):
         return str(self.user)
 
+
 class BlogPost(models.Model):
     id = models.AutoField(primary_key=True)
-    title=models.CharField(max_length=255)
-    author= models.ForeignKey(User, on_delete=models.CASCADE)
-    slug=models.CharField(max_length=130)
-    content=RichTextUploadingField()
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # slug = models.CharField(max_length=130)
+    slug = models.SlugField(max_length=200, allow_unicode=False)
+    content = RichTextUploadingField()
     image = models.ImageField(upload_to="profile_pics", blank=True, null=True)
-    dateTime=models.DateTimeField(auto_now_add=True)
-    
+    dateTime = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
-        return str(self.author) +  " Blog Title: " + self.title
-    
+        return str(self.author) + " Blog Title: " + self.title
+
     def get_absolute_url(self):
-        return reverse('blogs')
+        return reverse('home:blogs')
 
 
-    
 class Comment(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     blog = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
-    parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)   
+    parent_comment = models.ForeignKey(
+        'self', on_delete=models.CASCADE, null=True, blank=True)
     dateTime = models.DateTimeField(default=now)
- 
+
     def __str__(self):
-        return self.user.username +  " Comment: " + self.content
+        return self.user.username + " Comment: " + self.content
